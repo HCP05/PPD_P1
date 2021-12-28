8070a5deea666e61c7e943ed0056cbb1a27c4643
Leonard Pop <leonard.pop@stud.ubbcluj.ro>
2021-12-28 16:33:28 +0200

    Update test case

--- test_case_script.sql
+++ test_case_script.sql
@@ -8,12 +8,11 @@ UPDATE "sqlite_sequence" SET "seq" = 0 WHERE "name" = "Spectacole";
 UPDATE "sqlite_sequence" SET "seq" = 0 WHERE "name" = "Vanzari";
 
 INSERT INTO Sali(nr_locuri) VALUES
-	(50),
-	(1000),
+	(100),
+	(100),
 	(100);
 
 INSERT INTO Spectacole(id_sala, data_spectacol, titlu, pret_bilet) VALUES
-	(1, '01-01-1970', 'First', 123.9),
-	(1, '01-02-1970', 'Second', 321.9),
-	(2, '01-01-1970', 'Third', 23.3),
-	(3, '01-01-1970', 'Fourth', 123.4);
+	(1, '01-01-1970', 'S1', 100),
+	(1, '01-02-1970', 'S2', 200),
+	(2, '01-01-1970', 'S3', 150);
